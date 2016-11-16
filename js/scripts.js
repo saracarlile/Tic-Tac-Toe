3e492fd9b898269f5dee95cc7fcde8c963e0131e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-16 14:37:53 -0600

    progress

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,75 @@ var aiPlayerTurn = false;
 var board = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
+    [true, true, true]
 ]
 
-function updateMove() {
-  //  var winner = getWinner(board);
-    $('#indicator').text(aiPlayerTurn ? "AI's Move" : "Your move");
+function getWinner(board) {
+    console.log(board);
+    vals = [true, false];  // check to see "X" true won or "O" false won
+    var allNotNull = true; // if no squares are null it's a tie
+    for (var v = 0; v < vals.length; v++) {
+        var value = vals[v] // check true then false
+        // Check rows, columns, and diagonals
+        var diagonalComplete1 = true;
+        var diagonalComplete2 = true;
+        for (var i = 0; i < 3; i++) {
+            if (board[i][i] != value) {
+                diagonalComplete1 = false;
+            }
+            if (board[2 - i][i] != value) {
+                diagonalComplete2 = false;
+            }
+            var rowComplete = true;
+            var colComplete = true;
+            for (var j = 0; j < 3; j++) {
+                if (board[i][j] != value) {
+                    rowComplete = false;
+                }
+                if (board[j][i] != value) {
+                    colComplete = false;
+                }
+                if (board[i][j] == null) {
+                    allNotNull = false;
+                }
+            }
+            if (rowComplete || colComplete) {
+                return value ? 1 : 0; // return 1 means X won and return 0 means O won
+            }
+        }
+        if (diagonalComplete1 || diagonalComplete2) {
+            return value ? 1 : 0;   // return 1 means X won and return 0 means O won
+        }
+    }
+    if (allNotNull) {
+        return -1;  // return tie
+    }
+    return null;  // return null if no winner yet
 }
 
 
+
+
+
+
+function updateMove() {
+    var winner = getWinner(board);
+    if(winner !== null){
+        $('#indicator').text(' '); 
+         if(winner === 0){
+              $('#winner').text("O won!");
+         }
+         if(winner === 1){
+             $('#winner').text("X won!");
+         }
+         if(winner === -1){
+             $('#winner').text("It's a tie.");
+         }                    
+    }
+    else{
+        $('#indicator').text(aiPlayerTurn ? "AI's Turn" : "Your Turn");
+    }   
+}
 
 $(document).ready(function () {
 
@@ -38,16 +98,16 @@ $(document).ready(function () {
                 sh2.text('X').addClass('player');
                 $(this).removeClass('rows');
                 aiPlayerTurn = true;
-                    updateMove();
-                //    makeMove();
+                updateMove();
+                //   makeMove();
             }
             else {
                 board[row][col] = false; //set board to "O" at that locatio
                 sh2.text('O').addClass('player');
                 $(this).removeClass('rows');
                 aiPlayerTurn = true;
-                   updateMove();
-                //    makeMove();
+                updateMove();
+                //   makeMove();
             }
         }
     });
@@ -56,6 +116,7 @@ $(document).ready(function () {
 
 
 
-
-
 });
+
+// thanks to this blog post for helping me to understand
+//minmax search https://blog.vivekpanyam.com/how-to-build-an-ai-that-wins-the-basics-of-minimax-search
