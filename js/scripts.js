1c20dcdfcd16536912fdb5f81287ac42d5b9e515
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-16 12:31:36 -0600

    making progress on game

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,58 @@
+
+var player;
+var aiPlayerTurn = false;
+var board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+function updateMove() {
+  //  var winner = getWinner(board);
+    $('#indicator').text(aiPlayerTurn ? "AI's Move" : "Your move");
+}
+
+
+
 $(document).ready(function () {
-    var player;
+
     function showSecondGrid() {
-        $(".second-grid").show(); 
+        $('.second-grid').show();
     }
-    $("a").click(function () {
+    $('a').click(function () {
         var id = this.id;
         player = id === 'X' ? true : false; // player is true if "X" and false if "O"
-        $(".first-grid").hide("drop", { direction: "left" }, 2000, showSecondGrid);
+        $('.first-grid').hide('drop', { direction: 'left' }, 2000, showSecondGrid);
     });
+
+
+    $('.rows').click(function () {
+        var sh2 = $(this).find("h2");
+        var cell = sh2.attr("id");
+        console.log(cell);
+        var row = parseInt(cell[1])
+        var col = parseInt(cell[2])
+        if (!aiPlayerTurn) {
+            if (player === true) {
+                board[row][col] = true;  //set board to "X" at that location
+                sh2.text('X').addClass('player');
+                $(this).removeClass('rows');
+                aiPlayerTurn = true;
+                    updateMove();
+                //    makeMove();
+            }
+            else {
+                board[row][col] = false; //set board to "O" at that locatio
+                sh2.text('O').addClass('player');
+                $(this).removeClass('rows');
+                aiPlayerTurn = true;
+                   updateMove();
+                //    makeMove();
+            }
+        }
+    });
+
+
 
 
 
