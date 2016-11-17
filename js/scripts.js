ab9df0c779ebd5b2bcba16ff9549e88028c973e9
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-17 13:12:08 -0600

    change effect duration

--- js/scripts.js
+++ js/scripts.js
@@ -164,7 +164,7 @@ $(document).ready(function () {
     $('a').click(function () {
         var id = this.id;
         player = id === 'X' ? true : false; // player is true if "X" and false if "O"
-        $('.first-grid').hide('drop', { direction: 'left' }, 2000, showSecondGrid);
+        $('.first-grid').hide('drop', { direction: 'left' }, 1000, showSecondGrid);
     });
 
 
